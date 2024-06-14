20d86e102f950453a177b24f35dae981f2377300
Muhammed Risil <87575552+risil01@users.noreply.github.com>
2024-06-14 09:02:19 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,23 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "messi",
+        hint: "GOAT in football"
+    },    {
+        word: "risil",
+        hint: "most brilliant in the world"
+    },    {
+        word: "java",
+        hint: "programming language"
+    },    {
+        word: "dhiu",
+        hint: "islamic university in kerala"
+    },    {
+        word: "python",
+        hint: "programming language"
+    },
+
+]
